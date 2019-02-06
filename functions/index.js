f528c29713e6c85e41e5ac2eeea70d42c7fe1f42
Pranitha Nagasai <46835976+PranithaNagasai@users.noreply.github.com>
2019-02-06 15:12:07 +0530

    changed for css

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions'),
 	express = require('express'),
 	app = express(),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	admin = require('firebase-admin');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -10,6 +11,9 @@ app.use(bodyParser.urlencoded({
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
+admin.initializeApp(functions.config().firebase);
+var db = admin.firestore();
 
 app.get("/", (req, res) => {
 	res.render('main');
@@ -26,6 +30,32 @@ app.get("/home", (req, res) => {
 app.get("/sign", (req, res) => {
 	res.render('sign');
 });
+
+app.get('/send', (req, res) => {
+	//var schoolName = "Pranitha";
+	var usingname= req.query.uname;
+	var sch = db.collection("sign").doc(usingname);
+	sch.get().then((doc) => {
+		if (doc.exists) {
+			console.warn(usingname + " already exists.");
+			return;
+		} else {
+			var ob = {
+				'name': req.query.fname,
+				'addr':req.query.addr,
+				'phone':req.query.num,
+				'dob':req.query.dob,
+				'username':req.query.uname,
+				'password':req.query.pass
+			};
+			db.collection("sign").doc(usingname).set(ob);
+			return;
+		}
+	}).catch((err) => {
+		console.log(err);
+	});
+	res.send('Done');
+});6
 
 app.use((req, res, next) => {
 	res.status(404).render('404');
