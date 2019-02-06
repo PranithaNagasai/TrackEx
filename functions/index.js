0d81605fb706ea2509b52b4b7cf3b956de2cd5d0
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-02-06 11:45:24 +0530

    Refactor

---
+++ functions/index.js
@@ -0,0 +1,34 @@
+const functions = require('firebase-functions'),
+	express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+	res.render('main');
+});
+
+app.get("/login", (req, res) => {
+	res.render('logintrack');
+});
+
+app.get("/home", (req, res) => {
+	res.render('track_home');
+});
+
+app.get("/sign", (req, res) => {
+	res.render('sign');
+});
+
+app.use((req, res, next) => {
+	res.status(404).render('404');
+});
+
+exports.app = functions.https.onRequest(app);
