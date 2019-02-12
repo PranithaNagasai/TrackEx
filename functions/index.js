cb68b4d248473c88de2f39e2e14c033f16fcd93b
Nuha S Varier <nuhavarier@gmail.com>
2019-02-13 00:42:37 +0530

    Login + Signup f+ Profile working
    Co-authored-by: Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>

--- functions/index.js
+++ functions/index.js
@@ -4,18 +4,16 @@ const functions = require('firebase-functions'),
 	bodyParser = require('body-parser'),
 	admin = require('firebase-admin');
 
+admin.initializeApp(functions.config().firebase);
+var db = admin.firestore();
+
 app.use(bodyParser.json());
-app.use(express.static('public/css'));
-// app.use(express.static('public/img'));
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
-
-admin.initializeApp(functions.config().firebase);
-var db = admin.firestore();
 
 app.get("/", (req, res) => {
 	res.render('index');
@@ -24,38 +22,62 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
 	res.render('login');
 });
+
 app.get("/dash", (req, res) => {
 	res.render('dash');
 });
+
 app.get("/profile", (req, res) => {
 	res.render('profile');
 });
+
 app.get("/pwd", (req, res) => {
 	res.render('pwd');
 });
-app.get("/loginOTP", (req, res) => {
+
+app.get("/loginOTP",(req,res) =>{
 	res.render('loginOTP');
 });
 
 app.get("/home", (req, res) => {
-	var a = req.query.fusername;
-	var b = req.query.pass;
-	var query = {
-		'username': req.query.fusername,
-		'password': req.query.pass
-	};
-
-
-	var db = FirebaseFirestore.getInstance();
-	db.collection(sign)
-		.whereEqualTo(username, a)
-		.get()
-		.addOnCompleteListener(task => {
-			if (task.isSuccessful()) {
-				if (task.getResult().getDocuments().size() > 0) { }
+	var a= req.query.fusername;
+	var b= req.query.pass;
+	var query= {'username':req.query.fusername,
+				 'password':req.query.pass,
 			}
+	console.log(a);
+	console.log(b);
+	console.log(query.username);
+	var us=db.collection('sign').doc(a);
+	var pw=db.collection('sign').doc(b);
+			//var getDoc = pri.where("username", "==",req.query.fusername).get()
+			us.get()
+			  .then((doc) => {
+				if (!doc.exists) {
+				  res.redirect('main');
+				} else {
+				  //alert('Document data:', doc.data());
+				  checkpwd();
+				 // res.redirect('dash');
+				}
+			  })
+			  .catch(err => {
+				console.log('Error getting document', err);
+			  });
+
+     function checkpwd(){
+		 pw.get().then((doc1)=>{
+				if(doc1.exists){
+					res.redirect('dash');
+					return doc1;
+				}
+				else{
+					res.render('logintrack');
+				}
+		 }).catch((err) => {
+			console.error(err);
 		});
-	res.render('dash');
+	 }	
 });
 
 app.get("/sign", (req, res) => {
@@ -66,7 +88,7 @@ app.get("/sign", (req, res) => {
 
 app.get('/send', (req, res) => {
 	//var schoolName = "Pranitha";
-	var usingname = req.query.uname;
+	var usingname= req.query.uname;
 	var sch = db.collection("sign").doc(usingname);
 	sch.get().then((doc) => {
 		if (doc.exists) {
@@ -75,14 +97,13 @@ app.get('/send', (req, res) => {
 		} else {
 			var ob = {
 				'name': req.query.fname,
-				'addr': req.query.addr,
-				'phone': req.query.num,
-				'dob': req.query.dob,
-				'username': req.query.uname,
-				'password': req.query.pass
+				'addr':req.query.addr,
+				'phone':req.query.num,
+				'dob':req.query.dob,
+				'username':req.query.uname,
+				'password':req.query.pass
 			};
 			db.collection("sign").doc(usingname).set(ob);
-
 			console.log(ob.name);
 			//res.redirect('/login'); .update()
 			//document.getElementById('uname').value
@@ -91,7 +112,7 @@ app.get('/send', (req, res) => {
 	}).catch((err) => {
 		console.log(err);
 	});
-
+	
 });
 
 
